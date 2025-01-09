c91199893d68d5ce6c48e0742d3be5e78f37a26f
Thalison Aragão <thalissonaragao01@gmail.com>
2025-01-09 09:15:09 -0300

    Many-to-many association with JoinTable

--- course/src/main/java/com/aragao/course/entities/Product.java
+++ course/src/main/java/com/aragao/course/entities/Product.java
@@ -20,7 +20,9 @@ public class Product implements Serializable {
     private Double price;
     private String imgUrl;
 
-    @Transient //Impede do JPA interpreta
+    @ManyToMany
+    @JoinTable(name = "tb_product_category",joinColumns =
+    @JoinColumn(name = "product_id"),inverseJoinColumns = @JoinColumn(name = "category_id")) //inverseJoinColumns para definir chave estrangeira da outra tabela
     private Set<Category> categories = new HashSet<>();
 
     public Product(){}
