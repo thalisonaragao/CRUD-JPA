ab5ecbd054c3835bc1bceec69eed4392dc223f5b
Thalison Aragão <thalissonaragao01@gmail.com>
2025-01-10 18:05:49 -0300

    Exception handling - findById

--- course/src/main/java/com/aragao/course/services/UserService.java
+++ course/src/main/java/com/aragao/course/services/UserService.java
@@ -2,6 +2,7 @@ package com.aragao.course.services;
 
 import com.aragao.course.entities.User;
 import com.aragao.course.repositories.UserRepository;
+import com.aragao.course.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -18,7 +19,7 @@ public class UserService {
     }
     public User findById(Long id){
         Optional<User> obj = repository.findById(id);
-        return obj.get();
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
     public User insert (User obj){
         return repository.save(obj);
