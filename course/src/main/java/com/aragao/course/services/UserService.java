140b23d9ce4df90e047b12290d83352307e94bb1
Thalison Aragão <thalissonaragao01@gmail.com>
2025-01-10 18:37:55 -0300

    Exception handling - delete

--- course/src/main/java/com/aragao/course/services/UserService.java
+++ course/src/main/java/com/aragao/course/services/UserService.java
@@ -2,8 +2,11 @@ package com.aragao.course.services;
 
 import com.aragao.course.entities.User;
 import com.aragao.course.repositories.UserRepository;
+import com.aragao.course.services.exceptions.DatabaseException;
 import com.aragao.course.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -25,7 +28,13 @@ public class UserService {
         return repository.save(obj);
     }
     public void delete(Long id) {
-        repository.deleteById(id);
+        try {
+            repository.deleteById(id);
+        } catch (EmptyResultDataAccessException e){
+            throw new ResourceNotFoundException(id);
+        } catch (DataIntegrityViolationException e){
+            throw new DatabaseException(e.getMessage());
+        }
     }
     public User update(Long id, User obj){
         User entity = repository.getReferenceById(id); //Antes o nome era getOne()
